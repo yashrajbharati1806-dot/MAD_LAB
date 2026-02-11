c7facd35b96a4ab7d5d667995f9099683b176e36
yashrajbharati1806-dot <yashrajbharati1806@gmail.com>
2026-02-11 14:51:03 +0530

    comment in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mad_lab/register_page.dart';
 import 'package:mad_lab/utility.dart';
 import 'package:mad_lab/configurations.dart';
-
+//JBJHB
 void main() {
   runApp(const MyApp());
 }
